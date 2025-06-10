ad4a5c719babfc1d8ec2a5a3fed30391e36b8c0f
dpogreba <derek.pogrebamoore@carvana.com>
2025-06-10 13:23:03 -0700

    Update app version to 10.0 (since version 9.0 was already used)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.antbear.pwneyes"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "9.0"
+        versionCode = 10
+        versionName = "10.0"
         
         // Explicitly disable baseline profiles to fix installation issues
         ndk {
